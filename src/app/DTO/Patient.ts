d723a9a3f82d656cfc3c0d2739466ae746027de9
MaryneGuevel22 <78147665+MaryneGuevel22@users.noreply.github.com>
2022-09-21 11:02:48 +0200

    update patient

--- src/app/DTO/Patient.ts
+++ src/app/DTO/Patient.ts
@@ -1,3 +1,18 @@
-export class Patient{
-    
+import { MatLine } from "@angular/material/core"
+
+export class Patient {
+    id : String,
+    active : boolean,
+    name: Name,
+    adress: Adress,
+    gender: string
+
 }
+
+class name
+family: string
+given: string
+
+class adress 
+line: string
+city: string
--- src/app/service/service-patient.service.ts
+++ src/app/service/service-patient.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
+import { Patient } from '../DTO/Patient';
+import{Http} from '@angular/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicePatientService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getPatient():Observable<Patient>{
+
+    let url="https://fhir.alliance4u.io/api/patient/632ac2df634beb001b834dd6"
+
+    return this.http.get<Patient[]>(url);
+
+  }
+
+  
 }
